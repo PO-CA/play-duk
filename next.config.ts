77608ff78dcb79fb3dbc167ff61938618f2e3efd
kkh <kkhdevs@gmail.com>
2025-10-20 15:22:33 +0900

    init

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  // 이미지 최적화 설정
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
